18e9a5b215be2190fc017267987281e20ef35853
dak180 <dak180@users.sourceforge.net>
2025-10-05 11:03:44 -0400

    Add a delay time option.

--- unifi_remote_start.sh
+++ unifi_remote_start.sh
@@ -8,10 +8,25 @@ switchName="se8"
 keyPort="2"
 
 
+while getopts ":t:" OPTION; do
+	case "${OPTION}" in
+		t)
+			setTime="${OPTARG}"
+		;;
+		*)
+			true
+		;;
+	esac
+done
+
 # Must be run as root
 if [ ! "$(whoami)" = "root" ]; then
 	echo "Must be run as root." >&2
 	exit 1
+fi
+
+if [ ! -z "${setTime}" ] && [[ "${setTime}" =~ ^[0-9]+$ ]]; then
+	sleep "${setTime}"
 fi
 
 if ! ssh "${cloudKey}" true &> /dev/null; then
